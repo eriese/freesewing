241ed0d70150d2319cbd5de0041583efcd4678ea
Enoch Riese <enoch.riese@gmail.com>
2022-07-15 16:44:10 -0400

    add pattern titles back to swipe nav

--- sites/shared/components/navigation/primary.js
+++ sites/shared/components/navigation/primary.js
@@ -149,7 +149,7 @@ const SubLevel = ({ nodes={}, active }) => (
                {child.__slug === active ? <>&bull;</> : <>&deg;</>}
               </span>
               <span className={child.__slug === active ? 'font-bold' : ''}>
-                {child.__linktitle}
+                {child.__linktitle || child.__title}
               </span>
             </a>
           </Link>
